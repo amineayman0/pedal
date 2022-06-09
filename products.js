0423d904c3f57132e35aebc9c1cdcb695eb4b48b
amineayman0 <106935462+amineayman0@users.noreply.github.com>
2022-06-09 23:45:27 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: 'آلة التمرين بالدواسات الكهربائية مناسبة للذراعين و الساقين مع شاشة إلكترونية و لوحة تحكم Appareil D’exercice A Pédales Electrique Pour Bras Et Jambes Avec Afficheur Et Telecommande',
+		price1: 27800,
+		price: 25800,
+		description: 'دراجة تمرين صغيرة مصممة خصيصًا لتمارين الذراعين والساقين،وهي اختيارية للدواسة للأمام أو للخلف. 6 برامج تلقائية مدمجة،برنامج يدوي واحد،12 إعداد سرعة،سرعة قصوى: 80 دورة في الدقيقة. سهلة التجميع مع توفر جميع الملحقات ، في غضون دقائق قليلة ودون طلب أدوات. دواسة قابلة للتعديل بناءً على احتياجاتك. تصميم خفيف الوزن وقابل للطي. شاشة إلكترونية و لوحة تحكم.'   ,
+		phone: 0540643397,
+	},
+
+	
+];
